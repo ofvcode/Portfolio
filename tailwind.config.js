7785223fc94a288235d2d293b137e39751e83671
ofvcode <adminofv@proton.me>
2025-01-18 19:09:28 -0500

    compile style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     './scripts/**/*.{js,ts}', // Include any JavaScript or TypeScript files in the scripts folder
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       // Add custom styles or extend the theme here
       colors: {
