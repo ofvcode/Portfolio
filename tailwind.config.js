f4809228b96495ac89245eddba781b109ec74b7b
ofvcode <adminofv@proton.me>
2025-01-18 19:51:52 -0500

    Fixed bloated file organization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html', // Include the main HTML file
-    './scripts/**/*.{js,ts}', // Include any JavaScript or TypeScript files in the scripts folder
+    "./index.html", // Add the main HTML file to content
   ],
   theme: {
-    screens: {
-      'sm': '640px',
-      
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    },
     extend: {
-      // Add custom styles or extend the theme here
-      colors: {
-
+      animation: {
+        tilt: 'tilt 10s infinite linear', // Add custom tilt animation
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)", // Define keyframe at start, middle, and end
+          },
+          "25%": {
+            transform: "rotate(1deg)", // Define keyframe at 25%
+          },
+          "75%": {
+            transform: "rotate(-1deg)", // Define keyframe at 75%
+          },
+        },
       },
     },
   },
-  plugins: [
-    // Add any Tailwind plugins here, if needed
-  ],
+  plugins: [],
 };
