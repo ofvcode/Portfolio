d3c274389a6c639a14d083716126de1255d1ba60
ofvcode <adminofv@proton.me>
2025-02-01 21:28:17 -0500

    New portfolio made

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html", // Add the main HTML file to content
-  ],
-  theme: {
-    extend: {
-      animation: {
-        tilt: 'tilt 10s infinite linear', // Add custom tilt animation
-      },
-      keyframes: {
-        tilt: {
-          "0%, 50%, 100%": {
-            transform: "rotate(0deg)", // Define keyframe at start, middle, and end
-          },
-          "25%": {
-            transform: "rotate(1deg)", // Define keyframe at 25%
-          },
-          "75%": {
-            transform: "rotate(-1deg)", // Define keyframe at 75%
+    content: [
+      "./index.html", // Add the main HTML file to content
+    ],
+    theme: {
+      extend: {
+        animation: {
+          tilt: 'tilt 10s infinite linear', // Add custom tilt animation
+        },
+        keyframes: {
+          tilt: {
+            "0%, 50%, 100%": {
+              transform: "rotate(0deg)", // Define keyframe at start, middle, and end
+            },
+            "25%": {
+              transform: "rotate(1deg)", // Define keyframe at 25%
+            },
+            "75%": {
+              transform: "rotate(-1deg)", // Define keyframe at 75%
+            },
           },
         },
       },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+  };
